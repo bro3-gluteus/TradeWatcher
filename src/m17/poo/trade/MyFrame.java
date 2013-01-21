a3e7a5ac0dc161343a77dd52cc0561b30723a74d
bro3-gluteus <hip.bra3@gmail.com>
2013-01-22 01:11:37 +0900

    Look&Feelをデフォルトに戻した

--- src/m17/poo/trade/MyFrame.java
+++ src/m17/poo/trade/MyFrame.java
@@ -3,9 +3,6 @@ package m17.poo.trade;
 import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
 //共通のセッティング
 
@@ -14,15 +11,5 @@ public class MyFrame extends JFrame{
 	MyFrame(){
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setLayout(new BorderLayout());
-		
-		//Look&FeelをOSの物に。
-    try {
-      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-	  } catch (ClassNotFoundException e) {
-	  } catch (InstantiationException e) {
-	  } catch (IllegalAccessException e) {
-	  } catch (UnsupportedLookAndFeelException e) {
-	  }
-	  SwingUtilities.updateComponentTreeUI(this);
 	}
 }
